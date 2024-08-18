558c3deb4e24de52442cd5b0b6b4fd1ea3484c1a
ashwaniverma-github <ap8606574@gmail.com>
2024-08-18 19:42:16 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.devpage.in', 
+    generateRobotsTxt: true, // Generate robots.txt file
+    // ...other options
+  };
+  
