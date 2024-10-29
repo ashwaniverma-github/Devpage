26c428d39a5addf6d3dd4c5f6813e86276d03387
ashwaniverma-github <ap8606574@gmail.com>
2024-10-29 10:25:06 +0530

    footer

--- src/components/sm-components/footer.tsx
+++ src/components/sm-components/footer.tsx
@@ -3,11 +3,11 @@ export default function Footer(){
         <footer className="footer bg-slate-100 text p-10">
         <nav>
             <h6 className="footer-title" >Donation</h6>
-            <a href="https://razorpay.me/@devpage" className="link link-hover">Keep Devpage Free</a>
+            <a href="https://razorpay.me/@devpage" target="_blank" rel="noopener noreferrer" className="link link-hover">Keep Devpage Free</a>
         </nav>
         <nav>
             <h6 className="footer-title" >social</h6>
-            <a href="https://github.com/ashwaniverma-github/Devpage" className="link link-hover" >Github</a>
+            <a href="https://github.com/ashwaniverma-github/Devpage" target="_blank" rel="noopener noreferrer" className="link link-hover" >Github</a>
         </nav>
       </footer>
     )
